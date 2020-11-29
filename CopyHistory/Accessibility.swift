eb1f858f90d8e011595e46e10fed00e7281f5a09
Keith Smiley <keithbsmiley@gmail.com>
2020-11-28 20:32:43 -0800

    Add accessibility settings

---
+++ CopyHistory/Accessibility.swift
@@ -0,0 +1,20 @@
+import AppKit
+import Foundation
+
+func foo() {
+    let options: [NSString: Bool] = [
+        kAXTrustedCheckOptionPrompt.takeUnretainedValue(): true
+    ]
+
+    if AXIsProcessTrustedWithOptions(options as CFDictionary) {
+        return
+    }
+
+    let alert = NSAlert()
+    alert.messageText = "Enable Accessibility First"
+    alert.informativeText = "Find the system popup behind this one, click \"Open System Preferences\" and enable CopyHistory. Then launch CopyHistory again."
+    alert.alertStyle = .critical
+    alert.addButton(withTitle: "Quit")
+    alert.runModal()
+    NSApp.terminate(nil)
+}
